6da4fad583179cdba1458ea156b1119ddf18c2b1
SalOrak <99770300+SalOrak@users.noreply.github.com>
2025-08-02 14:02:34 +0200

    style(flake): indent

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
         nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
             system = "x86_64-linux";
             modules = [
-            ./configuration.nix
+                ./configuration.nix
             ];
         };
     };
