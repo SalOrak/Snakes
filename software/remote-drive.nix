a91caad548394bb2fd347f3df6782d755ec04dd0
SalOrak <99770300+SalOrak@users.noreply.github.com>
2025-09-30 17:55:12 +0200

    feat(remote-drive|script): scripted version

--- software/remote-drive.nix
+++ software/remote-drive.nix
@@ -1,91 +1,91 @@
 {
-  config,
-  pkgs,
-  ...
-}: {
-  environment.systemPackages = with pkgs; [
-    rclone
-  ];
+    config,
+        pkgs,
+        ...
+}: 
+let 
+    rclone-personal-script = pkgs.writeShellScriptBin "rclone-personal-script" ''
+        ${pkgs.rclone}/bin/rclone bisync ~/drive/personal/dev dev-personal:/
+        ${pkgs.rclone}/bin/rclone bisync ~/drive/personal/proton proton-personal:/
+    '';
 
-  #  Personal Drive Sync
-  systemd.user = {
-    services = {
-      rclone-personal-drive = {
-        description = "Bidirection sync Personal drives";
-        wantedBy = ["graphical-session.target" "network-online.target"];
-        after = ["graphical-session.target" "network-online.target"];
-        serviceConfig = {
-          Type = "oneshot";
-          ExecStart = ''
-            ${pkgs.rclone}/bin/rclone bisync ~/drive/personal/dev dev-personal:/
-            ${pkgs.rclone}/bin/rclone bisync ~/drive/personal/proton proton-personal:/
-          '';
+    rclone-work-script = pkgs.writeShellScriptBin "rclone-work-script" ''
+        ${pkgs.rclone}/bin/rclone bisync ~/drive/work/proven work-proven:/sync
+    '';
+in {
+    environment.systemPackages = with pkgs; [
+        rclone
+    ];
+
+#  Personal Drive Sync
+    systemd.user = {
+        services = {
+            rclone-personal-drive = {
+                description = "Bidirection sync Personal drives";
+                wantedBy = ["graphical-session.target" "network-online.target"];
+                after = ["graphical-session.target" "network-online.target"];
+                serviceConfig = {
+                    Type = "oneshot";
+                    ExecStart = "${rclone-personal-script}/bin/rclone-personal-script"
+                };
+            };
+            rclone-personal-drive--logout = {
+                description = "Bidirection sync Personal drives at logout";
+                before = ["exit.target"];
+                serviceConfig = {
+                    Type = "oneshot";
+                    ExecStart = "${rclone-personal-script}/bin/rclone-personal-script"
+                };
+            };
         };
-      };
-      rclone-personal-drive--logout = {
-        description = "Bidirection sync Personal drives at logout";
-        before = ["exit.target"];
-        serviceConfig = {
-          Type = "oneshot";
-          ExecStart = ''
-            ${pkgs.rclone}/bin/rclone bisync ~/drive/personal/dev dev-personal:/
-            ${pkgs.rclone}/bin/rclone bisync ~/drive/personal/proton proton-personal:/
-          '';
+
+        timers = {
+            rclone-personal-drive-boot = {
+                description = "Run personal rclone at boot and every 15 min.";
+                wantedBy = ["timers.target"];
+                timerConfig = {
+                    OnBooSec = "1m";
+                    OnUnitActiveSec = "15m";
+                    Unit = "rclone-personal-drive.service";
+                    Persistent = true;
+                };
+            };
         };
-      };
     };
 
-    timers = {
-      rclone-personal-drive-boot = {
-        description = "Run personal rclone at boot and every 15 min.";
-        wantedBy = ["timers.target"];
-        timerConfig = {
-          OnBooSec = "1m";
-          OnUnitActiveSec = "15m";
-          Unit = "rclone-personal-drive.service";
-          Persistent = true;
+#  Work Drive Sync
+    systemd.user = {
+        services = {
+            rclone-work-drive = {
+                description = "Bidirection sync work drives";
+                wantedBy = ["graphical-session.target" "network-online.target"];
+                after = ["graphical-session.target" "network-online.target"];
+                serviceConfig = {
+                    Type = "oneshot";
+                    ExecStart = "${rclone-work-script}/bin/rclone-work-script"
+                };
+            };
+            rclone-work-drive--logout = {
+                description = "Bidirection sync work drives at logout";
+                before = ["exit.target"];
+                serviceConfig = {
+                    Type = "oneshot";
+                    ExecStart = "${rclone-work-script}/bin/rclone-work-script"
+                };
+            };
         };
-      };
-    };
-  };
 
-  #  Work Drive Sync
-  systemd.user = {
-    services = {
-      rclone-work-drive = {
-        description = "Bidirection sync work drives";
-        wantedBy = ["graphical-session.target" "network-online.target"];
-        after = ["graphical-session.target" "network-online.target"];
-        serviceConfig = {
-          Type = "oneshot";
-          ExecStart = ''
-            ${pkgs.rclone}/bin/rclone bisync ~/drive/work/proven work-proven:/
-          '';
+        timers = {
+            rclone-work-drive-boot = {
+                description = "Run work rclone at boot and every 15 min.";
+                wantedBy = ["timers.target"];
+                timerConfig = {
+                    OnBooSec = "1m";
+                    OnUnitActiveSec = "15m";
+                    Unit = "rclone-work-drive.service";
+                    Persistent = true;
+                };
+            };
         };
-      };
-      rclone-work-drive--logout = {
-        description = "Bidirection sync work drives at logout";
-        before = ["exit.target"];
-        serviceConfig = {
-          Type = "oneshot";
-          ExecStart = ''
-            ${pkgs.rclone}/bin/rclone bisync ~/drive/work/proven work-proven:/
-          '';
-        };
-      };
     };
-
-    timers = {
-      rclone-work-drive-boot = {
-        description = "Run work rclone at boot and every 15 min.";
-        wantedBy = ["timers.target"];
-        timerConfig = {
-          OnBooSec = "1m";
-          OnUnitActiveSec = "15m";
-          Unit = "rclone-work-drive.service";
-          Persistent = true;
-        };
-      };
-    };
-  };
 }
