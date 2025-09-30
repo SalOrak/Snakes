d8e5221447548fa5a2c95b04f7167c42001e4786
SalOrak <99770300+SalOrak@users.noreply.github.com>
2025-09-30 18:40:58 +0200

    pkgs: configure mime types

---
+++ software/mime-types.nix
@@ -0,0 +1,15 @@
+{
+  pkgs,
+  config,
+  ...
+}: {
+  xdg.mimeApps = {
+    "application/pdf" = "zathura.desktop";
+    "application/msword" = "libreoffice-writer.desktop";
+    "application/vnd.openxmlformats-officedocument.wordprocessingml.document" = "libreoffice-writer.desktop";
+    "application/vnd.ms-excel" = "libreoffice-calc.desktop";
+    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" = "libreoffice-calc.desktop";
+    "audio/*" = "vlc.desktop";
+    "video/*" = "vlc.desktop";
+  };
+}
