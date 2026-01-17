241aa0378f864f3eb4d02e7065b26741ddb7369c
SalOrak <99770300+SalOrak@users.noreply.github.com>
2026-01-17 10:31:33 +0100

    ..

--- system/programs.nix
+++ system/programs.nix
@@ -1,40 +1,31 @@
 {
-	pkg,
-		config,
-		...
+  pkgs,
+  config,
+  ...
 }: {
-	programs.direnv = {
-		enable = true;
-		enableBashIntegration = true;
-		direnvcExtra = ''
-			: "${XDG_CACHE_HOME:="${HOME}/.cache"}"
-			declare -A direnv_layout_dirs
-			direnv_layout_dir() {
-				local hash path
-					echo "${direnv_layout_dirs[$PWD]:=$(
-							hash="$(sha1sum - <<< "$PWD" | head -c40)"
-							path="${PWD//[^a-zA-Z0-9]/-}"
-							echo "${XDG_CACHE_HOME}/direnv/layouts/${hash}${path}"
-							)}"
-			}
-		'';
-		nix-direnv = {
-			enable = true;
-			package = pkgs.nix-direnv;
-		};
-	};
+	environment.systemPackages = with pkgs; [
+		direnv
+			nix-direnv
+	];
 
-	programs.mtr.enable = true;
-	programs.gnupg = {
-		agent = {
-			enable = true;
-			enableSSHSupport = true;
-			settings = {
-				default-cache-ttl = 86400;
-				default-cache-ttl-ssh = 86400;
-				max-cache-ttl = 2592000;
-				max-cache-ttl-ssh = 2592000;
-			};
-		};
-	};
+  programs.direnv = {
+    enable = true;
+    enableBashIntegration = true;
+    loadInNixShell = true;
+    nix-direnv.enable = true;
+  };
+
+  programs.mtr.enable = true;
+  programs.gnupg = {
+    agent = {
+      enable = true;
+      enableSSHSupport = true;
+      settings = {
+        default-cache-ttl = 86400;
+        default-cache-ttl-ssh = 86400;
+        max-cache-ttl = 2592000;
+        max-cache-ttl-ssh = 2592000;
+      };
+    };
+  };
 }
