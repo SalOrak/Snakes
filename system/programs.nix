dce4c366a4e7a0b90795859c6bcca532277498e4
SalOrak <99770300+SalOrak@users.noreply.github.com>
2026-01-17 12:09:08 +0100

    fix: direnv. See comment

--- system/programs.nix
+++ system/programs.nix
@@ -3,15 +3,15 @@
   config,
   ...
 }: {
-	environment.systemPackages = with pkgs; [
-		direnv
-			nix-direnv
-	];
-
   programs.direnv = {
     enable = true;
     enableBashIntegration = true;
+    enableZshIntegration = false;
+    enableFishIntegration = false;
     loadInNixShell = true;
+    # This depends on `direnvrc` file hack in dotfiles repo.
+    # Check out the first comment
+    # See https://github.com/SalOrak/dotfiles/blob/main/files/direnv/direnvrc
     nix-direnv.enable = true;
   };
 
