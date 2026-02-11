11b90120ba33f92baad02158ba06668891f386d8
SalOrak <99770300+SalOrak@users.noreply.github.com>
2026-02-11 20:54:32 +0100

    udev: added qmk rules

--- system/udev.nix
+++ system/udev.nix
@@ -5,6 +5,8 @@
   ...
 }: {
   services.udev.extraRules =
+    lib.fileContents ./udev/50-qmk.rules
+    + "\n"
     lib.fileContents ./udev/69-microbit.rules
     + "\n"
     + lib.fileContents ./udev/69-probe-rs.rules;
