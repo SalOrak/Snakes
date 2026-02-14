f0945ad775b7939e5a9db05dc16a905ec553fcdc
SalOrak <99770300+SalOrak@users.noreply.github.com>
2026-02-14 21:11:13 +0100

    fix udev rules +

--- system/udev.nix
+++ system/udev.nix
@@ -7,7 +7,7 @@
   services.udev.extraRules =
     lib.fileContents ./udev/50-qmk.rules
     + "\n"
-    lib.fileContents ./udev/69-microbit.rules
+    + lib.fileContents ./udev/69-microbit.rules
     + "\n"
     + lib.fileContents ./udev/69-probe-rs.rules;
 }
