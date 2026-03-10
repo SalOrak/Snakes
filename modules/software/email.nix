9257bb0213e3bfc60e799263c315be7decb439b6
SalOrak <99770300+SalOrak@users.noreply.github.com>
2026-03-10 20:55:53 +0100

    mbsync: service + timer with notmuch

--- modules/software/email.nix
+++ modules/software/email.nix
@@ -4,7 +4,23 @@
   inputs,
   lib,
   ...
-}: {
+}: let
+  mbsync-notmuch-script = pkgs.writeShellScriptBin "mbsync-notmuch-script" ''
+    	MBSYNC=$(pgrep mbsync)
+    NOTMUCH=$(pgrep notmuch)
+
+    	if [ -n "$MBSYNC" -o -n "$NOTMUCH" ]; then
+    	echo "Already running one instance of mbsync or notmuch. Exiting..."
+    	exit 0
+    	fi
+
+    	echo "Deleting messages tagged as *deleted*"
+    	${pkgs.notmuch} search --format=text0 --output=files tag:deleted | xargs -0 --no-run-if-empty rm -v
+
+    	${pkgs.mbsync} -Va
+    	${pkgs.notmuch} new
+  '';
+in {
   options = {
     email.enable = lib.mkEnableOption "Enable local email";
     email.pkg = lib.mkOption {
@@ -13,6 +29,7 @@
       example = lib.literalExpression "pkgs.aerc";
       description = "The package to use for the local email client.";
     };
+    email.mbsync = lib.mkEnableOption "Enable Mbsync service";
     email.protonmail.enable = lib.mkEnableOption "Enable Protonmail service";
     email.protonmail.certPath = lib.mkOption {
       type = lib.types.path;
@@ -36,6 +53,33 @@
         pkgs.pandoc
       ];
     }
+    (lib.mkIf config.email.mbsync {
+      systemd.user = {
+        services = {
+          mbsync = {
+            description = "Mailbox sync with Notmuch integration.";
+            wantedBy = ["graphical-session.target" "network-online.target"];
+            after = ["graphical-session.target" "network-online.target"];
+            serviceConfig = {
+              Type = "oneshot";
+              ExecStart = "${mbsync-notmuch-script}/bin/mbsync-notmuch-script";
+            };
+          };
+        };
+        timers = {
+          mbsync = {
+            description = "Run mbsync every 3 min";
+            wantedBy = ["timers.target"];
+            timerConfig = {
+              OnStartupSec = "3m";
+              OnUnitActiveSec = "3m";
+              Unit = "mbsync.service";
+              Persistent = false;
+            };
+          };
+        };
+      };
+    })
     (lib.mkIf config.email.protonmail.enable {
       services.protonmail-bridge = {
         enable = true;
