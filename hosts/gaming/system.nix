64ae5e4be3bc73d3a40d827bc133b84efdb6e707
SalOrak <99770300+SalOrak@users.noreply.github.com>
2026-06-13 18:05:40 +0200

    gaming: from plasma to sway again:)

--- hosts/gaming/system.nix
+++ hosts/gaming/system.nix
@@ -48,8 +48,41 @@
       };
       videoDrivers = ["nvidia"];
     };
-    desktopManager.plasma6.enable = true;
+    # desktopManager.plasma6.enable = true;
   };
+
+  programs.sway = {
+    enable = true;
+    wrapperFeatures.gtk = true;
+    extraPackages = with pkgs; [
+      swaylock
+      swayidle
+      wl-clipboard
+      wf-recorder
+      grim
+      sway-contrib.grimshot
+      slurp
+      wofi
+
+      # XDG Fix for waybar
+      waybar
+      xdg-utils
+      xdg-user-dirs
+      xdg-dbus-proxy
+      # xdg-desktop-portal
+      # xdg-desktop-portal-wlr
+      # xdg-desktop-portal-gnome
+    ];
+    extraSessionCommands = ''
+      export SDL_VIDEODRIVER=wayland
+      export QT_QPA_PLATFORM=wayland
+      export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
+      export _JAVA_AWT_WM_NONREPARENTING=1
+      export MOZ_ENABLE_WAYLAND=1
+    '';
+  };
+
+  programs.waybar.enable = true;
 
   hardware.enableAllFirmware = true;
   hardware.graphics.enable = true;
